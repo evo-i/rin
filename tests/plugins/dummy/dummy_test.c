1e11cc4490900884ae8862a4a55a56c96e0b7256
evo <149657734+evo-i@users.noreply.github.com>
2025-10-22 15:50:18 +0200

    Path to the plugin now replaced with args on test

--- tests/plugins/dummy/dummy_test.c
+++ tests/plugins/dummy/dummy_test.c
@@ -1,5 +1,6 @@
 #include <glib.h>
 #include <gmodule.h>
+#include <stdio.h>
 
 #include <windows.h>
 
@@ -18,50 +19,6 @@ typedef struct dummy_win32_fixture_s {
   void *p_instance;
 } dummy_win32_fixture_t;
 
-static gchar *
-build_plugin_filename(void) {
-#ifdef G_OS_WIN32
-  return g_strdup("libdummy_plugin.dll");
-#else
-  return g_strdup("libdummy_plugin.so");
-#endif
-}
-
-static gchar *
-try_open_from_dir(const char *dir) {
-  gchar *filename = build_plugin_filename();
-  gchar *path = g_build_filename(dir, filename, NULL);
-  g_free(filename);
-
-  GModule *mod = g_module_open(path, G_MODULE_BIND_LOCAL);
-  if (mod) {
-    g_module_close(mod);
-    return g_strdup(path);
-  }
-  g_free(path);
-  return NULL;
-}
-
-static gchar *
-find_plugin_path_from_env(void) {
-  const char *env = g_getenv("RIN_PLUGIN_PATH");
-  g_warning("RIN_PLUGIN_PATH=%s", env);
-  if (!env || *env == '\0')
-    return NULL;
-
-  gchar **tokens = g_strsplit(env, G_SEARCHPATH_SEPARATOR_S, 0);
-  gchar *result = NULL;
-  for (gchar **p = tokens; *p != NULL; ++p) {
-    gchar *candidate = try_open_from_dir(*p);
-    if (candidate) {
-      result = candidate;
-      break;
-    }
-  }
-  g_strfreev(tokens);
-  return result;
-}
-
 void
 dummy_win32_teardown(dummy_win32_fixture_t *fixture,
                      gconstpointer user_data) {
@@ -79,20 +36,11 @@ void
 dummy_win32_setup(dummy_win32_fixture_t *fixture,
                   gconstpointer user_data) {
   DUMMY_UNUSED(user_data);
-  /* Prefer paths from RIN_PLUGIN_PATH (search multiple dirs separated by
-   * G_SEARCHPATH_SEPARATOR), fall back to a relative build path. */
-
-  gchar *plugin_path = find_plugin_path_from_env();
-  if (!plugin_path) {
-    /* Try relative build path */
-    gchar *filename = build_plugin_filename();
-    plugin_path = g_build_filename("build", filename, NULL);
-    g_free(filename);
-  }
+  char const *plugin_path = user_data;
 
   g_assert_nonnull(plugin_path);
+
   fixture->hModule = LoadLibrary(plugin_path);
-  g_free(plugin_path);
 
   g_assert_nonnull(fixture->hModule);
 
@@ -160,21 +108,21 @@ main(int argc, char *argv[]) {
 
   g_test_add("/plugins/dummy/win32/interface",
     dummy_win32_fixture_t,
-    NULL,
+    argv[1],
     dummy_win32_setup,
     dummy_win32_get_interface,
     dummy_win32_teardown);
 
   g_test_add("/plugins/dummy/win32/info",
     dummy_win32_fixture_t,
-    NULL,
+    argv[1],
     dummy_win32_setup,
     dummy_win32_get_info,
     dummy_win32_teardown);
 
   g_test_add("/plugins/dummy/win32/info/fields",
     dummy_win32_fixture_t,
-    NULL,
+    argv[1],
     dummy_win32_setup,
     dummy_win32_check_info_fields,
     dummy_win32_teardown);
--- tests/plugins/dummy/dummy_test_gmodule.c
+++ tests/plugins/dummy/dummy_test_gmodule.c
@@ -15,50 +15,6 @@ typedef struct dummy_gmodule_fixture_s {
   void *p_instance;
 } dummy_gmodule_fixture_t;
 
-static gchar *
-build_plugin_filename(void) {
-#ifdef G_OS_WIN32
-  return g_strdup("libdummy_plugin.dll");
-#else
-  return g_strdup("libdummy_plugin.so");
-#endif
-}
-
-static gchar *
-try_open_from_dir(const char *dir) {
-  gchar *filename = build_plugin_filename();
-  gchar *path = g_build_filename(dir, filename, NULL);
-  g_free(filename);
-
-  GModule *mod = g_module_open(path, G_MODULE_BIND_LOCAL);
-  if (mod) {
-    g_module_close(mod);
-    return g_strdup(path);
-  }
-  g_free(path);
-  return NULL;
-}
-
-static gchar *
-find_plugin_path_from_env(void) {
-  const char *env = g_getenv("RIN_PLUGIN_PATH");
-  g_warning("RIN_PLUGIN_PATH=%s", env);
-  if (!env || *env == '\0')
-    return NULL;
-
-  gchar **tokens = g_strsplit(env, G_SEARCHPATH_SEPARATOR_S, 0);
-  gchar *result = NULL;
-  for (gchar **p = tokens; *p != NULL; ++p) {
-    gchar *candidate = try_open_from_dir(*p);
-    if (candidate) {
-      result = candidate;
-      break;
-    }
-  }
-  g_strfreev(tokens);
-  return result;
-}
-
 void
 dummy_gmodule_teardown(dummy_gmodule_fixture_t *fixture,
                        gconstpointer user_data) {
@@ -78,18 +34,10 @@ dummy_gmodule_setup(dummy_gmodule_fixture_t *fixture,
 
   /* Prefer paths from RIN_PLUGIN_PATH (search multiple dirs separated by
    * G_SEARCHPATH_SEPARATOR), fall back to a relative build path. */
-  gchar *plugin_path = find_plugin_path_from_env();
-  if (!plugin_path) {
-    /* Try relative build path */
-    gchar *filename = build_plugin_filename();
-    plugin_path = g_build_filename("build", "plugins", "dummy", filename, NULL);
-    g_free(filename);
-  }
-
+  char const *plugin_path = user_data;
   g_assert_nonnull(plugin_path);
 
   fixture->module = g_module_open(plugin_path, G_MODULE_BIND_LOCAL);
-  g_free(plugin_path);
   g_assert_nonnull(fixture->module);
 
   typedef rin_plugin_interface_t const *(*get_interface_func_t)(void);
@@ -162,21 +110,21 @@ main(int argc, char *argv[]) {
 
   g_test_add("/plugins/dummy/gmodule/interface",
     dummy_gmodule_fixture_t,
-    NULL,
+    argv[1],
     dummy_gmodule_setup,
     dummy_gmodule_get_interface,
     dummy_gmodule_teardown);
 
   g_test_add("/plugins/dummy/gmodule/info",
     dummy_gmodule_fixture_t,
-    NULL,
+    argv[1],
     dummy_gmodule_setup,
     dummy_gmodule_get_info,
     dummy_gmodule_teardown);
 
   g_test_add("/plugins/dummy/gmodule/info/fields",
     dummy_gmodule_fixture_t,
-    NULL,
+    argv[1],
     dummy_gmodule_setup,
     dummy_gmodule_check_info_fields,
     dummy_gmodule_teardown);
